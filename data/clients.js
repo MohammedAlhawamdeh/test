4a0816a09c390ea24ccd35e8b5afc0ee11fd24f0
Mohammed Alhawamdeh <hawamdeh9393@gmail.com>
2022-03-29 22:30:50 +0100

    adding data

---
+++ data/clients.js
@@ -0,0 +1,171 @@
+export const clients = [
+    {
+        clientId: 1,
+        clientName: "Client 1",
+        logo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.seoclerk.com%2Fpics%2F000%2F748%2F061%2Fbd1ddcf20243b5366e14524b6b79c773.png&f=1&nofb=1",
+        description: "description of client1",
+        stackTech: ["React", "Typescript"],
+        projects: [
+            {
+                projectId: 1,
+                projectName: "project1",
+                description: "description of project1",
+                lead: "Arsen Lowen",
+                start: "10/01/2020",
+                end: null,
+                clientContact: "Lin Que",
+                roles: [
+                    {
+                        PD: {
+                            quantityTotal: 4,
+                            enrolled: [
+                                {
+                                    andiId: 1,
+                                    name: "John Foo",
+                                    squad: "Spark",
+                                    role: "PD",
+                                    level: 4,
+                                    andTitle: "Foodie",
+                                    photo:
+                                        "https://c.pxhere.com/photos/57/7a/person_serious_shirt_indian_man_male_profile_sitting-484563.jpg!d",
+                                    pronouns: "he/him",
+                                },
+                                {
+                                    andiId: 3,
+                                    name: "Antonio Banderas",
+                                    squad: "Spark",
+                                    role: "PD",
+                                    level: 5,
+                                    andTitle: "dog dad",
+                                    photo:
+                                        "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.huffpost.com%2Fgen%2F2325240%2Fimages%2Fo-ANGRY-OLDER-PERSON-facebook.jpg&f=1&nofb=1",
+                                    pronouns: "he/him",
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        PA: {
+                            quantityTotal: 2,
+                            enrolled: [
+                                {
+                                    andiId: 2,
+                                    name: "Blue April",
+                                    squad: "Spark",
+                                    role: "PA",
+                                    level: 3,
+                                    andTitle: "Gardener",
+                                    photo:
+                                        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn-img.health.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Flarge_16_9%2Fpublic%2Fstyles%2Fmain%2Fpublic%2Fgettyimages-200527258-007_1.jpg%3Fitok%3DqJADjltR&f=1&nofb=1",
+                                    pronouns: "she/her",
+                                },
+                            ],
+                        },
+                    },
+                ],
+            },
+            {
+                projectId: 2,
+                projectName: "project2",
+                description: "description of project2",
+                lead: "Jensen Ackles",
+                start: "24/05/2021",
+                end: null,
+                clientContact: "Christian Bale",
+                roles: [
+                    {
+                        PD: {
+                            quantityTotal: 2,
+                            enrolled: [
+                                {
+                                    andiId: 3,
+                                    name: "Antonio Banderas",
+                                    squad: "Spark",
+                                    role: "PD",
+                                    level: 5,
+                                    andTitle: "dog dad",
+                                    photo:
+                                        "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.huffpost.com%2Fgen%2F2325240%2Fimages%2Fo-ANGRY-OLDER-PERSON-facebook.jpg&f=1&nofb=1",
+                                    pronouns: "he/him",
+                                },
+                                {
+                                    andiId: 4,
+                                    name: "Jessica Alba",
+                                    squad: "Spark",
+                                    role: "PD",
+                                    level: 3,
+                                    andTitle: "coffee affecionado",
+                                    photo:
+                                        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fblog.careacademy.com%2Fhubfs%2Fbigstock-Portrait-of-a-sick-elderly-wom-95655380.jpg&f=1&nofb=1",
+                                    pronouns: "she/her",
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        PA: {
+                            quantityTotal: 2,
+                            enrolled: [
+                                {
+                                    andiId: 5,
+                                    name: "Lauren Ambrose",
+                                    squad: "Spark",
+                                    role: "PA",
+                                    level: 3,
+                                    andTitle: "Actress",
+                                    photo:
+                                        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmiro.medium.com%2Fmax%2F909%2F1*_iikfMGYF3RH4OZ0yeQYnQ.png&f=1&nofb=1",
+                                    pronouns: "she/her",
+                                },
+                            ],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        clientId: 2,
+        clientName: "Client 2",
+        logo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F12%2FKubota-Logo-2010-present.png&f=1&nofb=1",
+        description: "description of client2",
+        stackTech: ["Angular", "Java"],
+        projects: [
+            {
+                projectId: 3,
+                projectName: "project3",
+                description: "description of project3",
+                lead: "Kathy Bates",
+                start: "01/08/2021",
+                end: null,
+                clientContact: "Kristin Bauer van Straten",
+                roles: [
+                    {
+                        PD: {
+                            quantityTotal: 4,
+                            enrolled: [
+                                {
+                                    andiId: 6,
+                                    name: "Sean Bean",
+                                    squad: "Spark",
+                                    role: "PD",
+                                    level: 4,
+                                    andTitle: "Producer",
+                                    photo:
+                                        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcf.girlsaskguys.com%2Fq3153686%2Fprimary-share.png%3F79&f=1&nofb=1",
+                                    pronouns: "he/him",
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        PA: {
+                            quantityTotal: 2,
+                            enrolled: [],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+];
