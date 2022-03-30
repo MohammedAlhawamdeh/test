abe85977922f5c039766fa37eab06f4147ee864f
Mohammed Alhawamdeh <hawamdeh9393@gmail.com>
2022-03-30 14:50:38 +0100

    dealing with data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import { clients } from './data/clients'
 import './App.css';
 
-function App() {
-  console.log('clients', clients)
+const App = () => {
   return (
-    <>
+    clients.map((client) => {
+      return (
+        <>
+          <p>{client.clientName}</p>
+          <p>{client.description}</p>
+          <img src={client.logo} alt="" />
+          {
+            client.stackTech.map(st => {
+              return (
+                <span>{`${st} `}</span>
+              )
+            })
+          }
+          {
 
-    </>
-  );
+          }
+        </>
+      )
+    })
+  )
 }
 
 export default App;
